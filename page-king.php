f309dfca722c5e6dd3c26d3ad35a94a5e0209ed4
ruvick <rudikov.web@ya.ru>
2021-02-25 14:48:35 +0300

    Поправил Породы

--- page-king.php
+++ page-king.php
@@ -1,16 +1,9 @@
 <?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- * Template Name: Бишон Фризе
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package dog
- */
+
+/*
+Template Name: Шаблон Порода Кавалер Кинг
+Template Post Type: page
+*/
 
 get_header();
 ?>
@@ -29,14 +22,12 @@ get_header();
       <div class="container">
         <h1 class="section-title"><?php the_title();?></h1>
         <div class="entry-dog-content">
-          <div class="entry-dog-content__photo" style="background-image: url(<?php echo get_template_directory_uri();?>/img/dog-2-2.jpg)"></div>
+          <div class="entry-dog-content__photo" style="background-image: url(<?php echo get_template_directory_uri();?>/img/dog-1-1.jpg)"></div>
           <div class="entry-dog-content__text">
             <p>
-              Бишон-фризе отличается превосходным характером. Это идеальная собака-компаньон: дружелюбная, преданная, 
-              обожающая общение со своей человеческой семьей. Привязанность к людям заставляет бишон-фриза находиться преимущественно в помещении. 
-              Бишон-фризе всегда готов поиграть, погулять или просто полежать возле вас. Позитивный настрой делает эту собаку идеальным другом 
-              как для молодежи, так и для пожилых людей. Это милое существо обожает быть центром внимания и удивительно легко усваивает 
-              специальные навыки.
+              Как и все спаниели, кинг чарльзы веселые и общительные собаки. Отличные компаньоны. Славятся своим хорошим отношением к детям. 
+              К другим животным относятся положительно, общительны и дружелюбны при контакте с посторонними людьми. Легко поддаются дрессировкам и 
+              беспрекословно слушаются хозяина.
             </p>
           </div>
         </div>
